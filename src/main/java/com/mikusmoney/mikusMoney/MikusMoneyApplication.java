9d6fd7ddbc6a9ba548f44b84035da589ced780cb
Jud1714 <juandiegolopezarias07@gmail.cm>
2025-12-08 21:46:18 -0500

    Setting up Miku's Money backend :D

---
+++ src/main/java/com/mikusmoney/mikusMoney/MikusMoneyApplication.java
@@ -0,0 +1,23 @@
+package com.mikusmoney.mikusMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikusMoneyApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(MikusMoneyApplication.class, args);
+
+        System.out.println("======================================");
+        System.out.println("Fan: HI MIKU I'M A FAN!");
+        System.out.println("Miku: Hi! Thanks for your support");
+        System.out.println("Fan: :D");
+        System.out.println("Miku: Send me nudes :D");
+        System.out.println("Fan: Miku noo D:");
+        System.out.println("======================================");
+
+    }
+
+}
---
+++ src/test/java/com/mikusmoney/mikusMoney/MikusMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikusmoney.mikusMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikusMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
