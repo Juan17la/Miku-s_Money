5b7fd6ff0b1527c85bd4dbca68b36f445cc667e3
juan17la <juandiegolopezarias07@gmail.com>
2025-12-21 11:45:49 -0500

    creating credential repository

---
+++ src/main/java/com/mikusmoney/mikusMoney/repository/CredentialRepository.java
@@ -0,0 +1,19 @@
+package com.mikusmoney.mikusMoney.repository;
+
+import com.mikusmoney.mikusMoney.entity.Credential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CredentialRepository extends JpaRepository<Credential, Long> {
+    
+    Optional<Credential> findByEmail(String email);
+    
+    Optional<Credential> findByPhoneNumber(String phoneNumber);
+    
+    boolean existsByEmail(String email);
+    
+    boolean existsByPhoneNumber(String phoneNumber);
+}
