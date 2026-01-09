5029f50e6f71104413ed875cb2b9f95207354c53
juan17la <juandiegolopezarias07@gmail.com>
2026-01-09 10:24:43 -0500

    Rename goalAmount field to goal in SavingsPigCreationRequest for consistency

--- src/main/java/com/mikusmoney/mikusMoney/dto/savingsDTOs/SavingsPigCreationRequest.java
+++ src/main/java/com/mikusmoney/mikusMoney/dto/savingsDTOs/SavingsPigCreationRequest.java
@@ -21,7 +21,7 @@ public class SavingsPigCreationRequest {
     @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
     @Digits(integer = 17, fraction = 2, message = "Invalid amount format")
-    private BigDecimal goalAmount;
+    private BigDecimal goal;
 
     @NotBlank(message = "Name is required")
     @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
