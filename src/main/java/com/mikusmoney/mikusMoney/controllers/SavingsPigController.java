3706241a72a557f29ad811fff7f0f4625d61cffe
juan17la <juandiegolopezarias07@gmail.com>
2026-01-09 10:24:48 -0500

    Add SavingsPigController for handling savings pig creation requests

---
+++ src/main/java/com/mikusmoney/mikusMoney/controllers/SavingsPigController.java
@@ -0,0 +1,32 @@
+package com.mikusmoney.mikusMoney.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigCreationRequest;
+import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigResponse;
+import com.mikusmoney.mikusMoney.services.SavingsPigService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RestController
+@RequestMapping("/api/v0.1/savings/pig/")
+@RequiredArgsConstructor
+public class SavingsPigController {
+    
+    private final SavingsPigService savingsPigService;
+
+    @PostMapping("create")
+    public ResponseEntity<SavingsPigResponse> createSavingsPig(@Valid @RequestBody SavingsPigCreationRequest request) {
+        
+        return ResponseEntity.ok(savingsPigService.createSavingsPig(request));
+    }
+    
+}
