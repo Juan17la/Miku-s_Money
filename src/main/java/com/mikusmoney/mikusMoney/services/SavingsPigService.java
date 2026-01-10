8a5df1402b6391871582a6ad2f48b84d0adf943f
juan17la <juandiegolopezarias07@gmail.com>
2026-01-10 12:06:37 -0500

    Add getSavingsPigs method to SavingsPigService for retrieving user's savings pigs

--- src/main/java/com/mikusmoney/mikusMoney/services/SavingsPigService.java
+++ src/main/java/com/mikusmoney/mikusMoney/services/SavingsPigService.java
@@ -1,10 +1,13 @@
 package com.mikusmoney.mikusMoney.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigCreationRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigResponse;
 import com.mikusmoney.mikusMoney.services.operations.CreateSavingsPigOperation;
+import com.mikusmoney.mikusMoney.services.operations.GetSavingsPigsOperation;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +17,13 @@ public class SavingsPigService {
     
 
     private final CreateSavingsPigOperation createSavingsPig;
+    private final GetSavingsPigsOperation getSavingsPigsOperation;
     
     public SavingsPigResponse createSavingsPig(SavingsPigCreationRequest request) {
         return createSavingsPig.execute(request);
     }
 
+    public List<SavingsPigResponse> getSavingsPigs() {
+        return getSavingsPigsOperation.execute(null);
+    }
 }
